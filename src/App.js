e88e0c312b144619aa0d28b768205b677989e221
Ashna Malhotra <ashna.malhotra@appirio.com>
2020-04-06 00:31:52 +0530

    fetch location

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { latitude: null, error: null };
+  }
+
+  componentDidMount() {
+    //fetch location
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+        });
+      },
+      (err) => {
+        this.setState({
+          error: err.message,
+        });
+      },
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.latitude ?
+            `Latitude : ${this.state.latitude}` :
+            this.state.error ?
+              `Error: ${this.state.error}` :
+              'Fetching...'
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
